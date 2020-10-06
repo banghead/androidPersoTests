cd0a1fb15673e740b1997392c9486b545b866d0e
banghead <florian.aubin@laposte.net>
2020-10-06 09:24:52 +0200

    Module + AAR

--- app/src/main/java/com/example/premierandroid/activities/MainActivity.java
+++ app/src/main/java/com/example/premierandroid/activities/MainActivity.java
@@ -405,22 +405,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        try {
-//            super.onActivityResult(requestCode, resultCode, data);
-//
-//            if (requestCode == REQUEST_CODE  && resultCode  == RESULT_OK) {
-//
-//                String requiredValue = data.getStringExtra("key");
-//            }
-//        } catch (Exception ex) {
-//            Toast.makeText(Activity.this, ex.toString(),
-//                    Toast.LENGTH_SHORT).show();
-//        }
-//
-//    }
-
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
---
+++ mylibraryaaaaaaa/src/main/java/com/example/mylibraryaaaaaaa/ClassTest.java
@@ -0,0 +1,23 @@
+package com.example.mylibraryaaaaaaa;
+
+public class ClassTest {
+
+    private int number;
+
+    public ClassTest(int number) {
+        this.number = number;
+    }
+
+
+    public int leDouble(int i){
+        return i*i;
+    }
+
+    public String ditBonjour(){
+        return "Bonjour !!!";
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
